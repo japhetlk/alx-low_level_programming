a9de002ebce5da18ee975e73383faf47fe7aa663
japhetlk <japhforwork@gmail.com>
2021-02-05 09:31:06 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
 
 
-
-/**
-
- * main - prints a text
-
- * Return: Always 0 (success)
-
- */
-
-
-
 int main(void)
 
 {
 
-  puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle\n");
 
   return (0);
 
