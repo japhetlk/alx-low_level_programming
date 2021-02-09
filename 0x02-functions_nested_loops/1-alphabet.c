83987b926f230687b293b67993934e55064abfff
japhetlk <japhforwork@gmail.com>
2021-02-09 08:32:48 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case followed by a new line
+ */
+
+void print_alphabet(void)
+{
+  char letter = 'a';
+
+  letter = 'a';
+  while (letter <= 'z')
+    {
+      _putchar(letter);
+      letter++;
+    }
+  _putchar('\n');
+
+}
