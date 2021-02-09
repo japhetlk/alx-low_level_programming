82af2419e0990452bf92d8167339aaddd0f79001
japhetlk <japhforwork@gmail.com>
2021-02-09 08:05:46 +0000

    nested_loop

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,27 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar -writes the character c to stdout
+ * @c: The input that will be printed
+ * Return: 1 on succes and -1 on error
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet(void) - prints the alphabet, in lowercase, followed by a new
+ * line
+ */
+
+void print_alphabet_x10(void);
+
+/**
+ * _islower - check lower case
+ * @c: a character to be checked
+ * Return: 1 if lower case othewise 0
+ */
+
+int _islower(int n);
+
+#endif
