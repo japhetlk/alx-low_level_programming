bc0977246ac7b44dbb3945049129bd56e3b74056
japhetlk <japhforwork@gmail.com>
2021-02-21 03:57:21 +0000

    code

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+
+
+/**
+
+ * puts_half - prints half of a string followed by a new line
+
+ * @str: the argument
+
+ */
+
+
+
+void puts_half(char *str)
+
+{
+
+  int x;
+
+  
+
+  x = 0;
+
+  while (str[x])
+
+    x++;
+
+  if (x % 2 != 0)
+
+    x = (x + 1) / 2; /*Add 1: decrease length of odd string half*/
+
+  else
+
+    x = x / 2;
+
+  while (str[x])
+
+    {
+
+      _putchar(str[x]);
+
+      x++;
+
+    }
+
+  _putchar('\n');
+
+}
